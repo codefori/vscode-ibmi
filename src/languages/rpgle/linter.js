8356f3f6c7c2b6fb0c42667e63aef1826d09af38
Liam Allan <liam@worksofbarry.com>
2021-03-10 16:17:56 -0500

    Optional RPGLE features

--- src/languages/rpgle/linter.js
+++ src/languages/rpgle/linter.js
@@ -1,5 +1,9 @@
 
+const path = require("path");
 const vscode = require(`vscode`);
+
+const instance = require(`../../Instance`);
+const Configuration = require(`../../api/Configuration`);
 
 module.exports = class RPGLinter {
   /**
@@ -15,33 +19,59 @@ module.exports = class RPGLinter {
     /** @type {Declaration[]} */
     this.structs = [];
 
+    /** @type {{[path: string]: string[]}} */
+    this.copyBooks = {};
+
     context.subscriptions.push(
       this.linterDiagnostics,
 
       vscode.workspace.onDidChangeTextDocument((event) => {
-        if (event.document.languageId === `rpgle`) {
-          const text = event.document.getText();
-          if (text.startsWith(`**FREE`)) {
-            this.linterDiagnostics.set(event.document.uri, this.parseFreeFormatDocument(text, {
-              indent: Number(vscode.window.activeTextEditor.options.tabSize)
-            }));
+        if (Configuration.get(`rpgleIndentationEnabled`)) {
+          if (event.document.languageId === `rpgle`) {
+            const text = event.document.getText();
+            if (text.startsWith(`**FREE`)) {
+              this.linterDiagnostics.set(event.document.uri, this.parseFreeFormatDocument(text, {
+                indent: Number(vscode.window.activeTextEditor.options.tabSize)
+              }));
+            }
           }
         }
       }),
 
       vscode.languages.registerHoverProvider({language: `rpgle`}, {
         provideHover: (document, position, token) => {
-          const range = document.getWordRangeAtPosition(position);
-          const word = document.getText(range).toUpperCase();
+          if (Configuration.get(`rpgleContentAssistEnabled`)) {
+            const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range).toUpperCase();
 
-          const procedure = this.procedures.find(proc => proc.name.toUpperCase() === word.toUpperCase());
+            const procedure = this.procedures.find(proc => proc.name.toUpperCase() === word.toUpperCase());
 
-          if (procedure) {
-            return new vscode.Hover(
-              new vscode.MarkdownString(
-                `\`${procedure.keywords.join(` `)}\`\n${procedure.comments}\n\n${procedure.subItems.map(parm => `* \`${parm.name} ${parm.keywords.join(` `)}\` ${parm.comments}`).join(`\n`)}`
+            if (procedure) {
+              let retrunValue = procedure.keywords.filter(keyword => keyword !== `EXTPROC`);
+              if (retrunValue.length === 0) retrunValue = [`void`];
+
+              const markdown = `\`\`\`vb\n${procedure.name}(\n  ${procedure.subItems.map(parm => `${parm.name}: ${parm.keywords.join(` `)}`).join(`,\n  `)}\n): ${retrunValue.join(` `)}\n\`\`\` \n` +
+              `\n\n` +
+              procedure.subItems.map(parm => `*@param* ${parm.name.replace(new RegExp(`\\*`, `g`), `\\*`)} ${parm.comments}`).join(`\n\n`) +
+              `\n\n*@returns* ${retrunValue.join(` `)}`;
+
+              return new vscode.Hover(
+                new vscode.MarkdownString(
+                  markdown
+                )
+              );
+            }
+
+            const linePieces = document.lineAt(position.line).text.trim().split(` `);
+            if ([`/COPY`, `/INCLUDE`].includes(linePieces[0].toUpperCase())) {
+              const {type, memberPath, finishedPath} = this.getPathInfo(document.uri, linePieces[1]);
+
+              return new vscode.Hover(
+                new vscode.MarkdownString(
+                  `\`'${finishedPath}'\` (${type})`
+                )
               )
-            );
+            }
           }
 
           return null;
@@ -67,18 +97,22 @@ module.exports = class RPGLinter {
 
       vscode.workspace.onDidSaveTextDocument((event) => {
         if (event.languageId === `rpgle`) {
-          const text = event.getText();
-          if (text.startsWith(`**FREE`)) {
-            this.getDocs(text);
+          if (Configuration.get(`rpgleContentAssistEnabled`)) {
+            const text = event.getText();
+            if (text.startsWith(`**FREE`)) {
+              this.getDocs(event.uri, text);
+            }
           }
         }
       }),
 
       vscode.workspace.onDidOpenTextDocument((event) => {
         if (event.languageId === `rpgle`) {
-          const text = event.getText();
-          if (text.startsWith(`**FREE`)) {
-            this.getDocs(text);
+          if (Configuration.get(`rpgleContentAssistEnabled`)) {
+            const text = event.getText();
+            if (text.startsWith(`**FREE`)) {
+              this.getDocs(event.uri, text);
+            }
           }
         }
       })
@@ -87,10 +121,130 @@ module.exports = class RPGLinter {
   }
 
   /**
+   * @param {vscode.Uri} workingUri Path being worked with
+   * @param {string} getPath IFS or member path to fetch
+   */
+  getPathInfo(workingUri, getPath) {
+    const config = instance.getConfig();
+
+    /** @type {string} */
+    let finishedPath = undefined;
+
+    /** @type {string[]} */
+    let memberPath = undefined;
+
+    /** @type {"streamfile"|"member"|undefined} */
+    let type = undefined;
+
+    if (getPath.includes(`/`) || getPath.includes(`.`)) {
+      type = `streamfile`;
+      //Fetch IFS
+
+      if (getPath.startsWith(`'`)) getPath = getPath.substring(1);
+      if (getPath.endsWith(`'`)) getPath = getPath.substring(0, getPath.length - 1);
+
+      if (getPath.startsWith(`/`)) {
+        //Get from root
+        finishedPath = getPath;
+      } 
+      else if (getPath.startsWith(`.`)) {
+        finishedPath = path.posix.join(config.homeDirectory, getPath);
+      } else {
+        finishedPath = path.posix.join(path.posix.dirname(workingUri.path), getPath);
+      }
+
+    } else {
+      //Fetch member
+      const getLib = getPath.split(`/`);
+      const getMember = getLib[getLib.length-1].split(`,`);
+      const workingPath = workingUri.path.split(`/`);
+      memberPath = [undefined, undefined, `QRPGLEREF`, undefined];
+
+      if (workingPath.length === 4) { //ASP not included
+        memberPath[1] = workingPath[1];
+      } else {
+        memberPath[0] = workingPath[1];
+        memberPath[1] = workingPath[2];
+      }
+
+      switch (getMember.length) {
+      case 1:
+        memberPath[3] = getMember[0];
+        break;
+      case 2:
+        memberPath[2] = getMember[0];
+        memberPath[3] = getMember[1];
+      }
+
+      if (getLib.length === 2) {
+        memberPath[1] = getLib[0];
+      }
+
+      finishedPath = memberPath.join(`/`);
+
+      type = `member`;
+    }
+
+    return {type, memberPath, finishedPath};
+  }
+
+  /**
+   * @param {vscode.Uri} workingUri Path being worked with
+   * @param {string} getPath IFS or member path to fetch
+   * @returns {Promise<string[]>}
+   */
+  async getContent(workingUri, getPath) {
+    const contentApi = instance.getContent();
+
+    let content;
+    let lines = undefined;
+    
+    // /** @type {string} */
+    // let finishedPath = undefined;
+
+    // /** @type {string[]} */
+    // let memberPath = undefined;
+
+    // /** @type {"streamfile"|"member"|undefined} */
+    // let type = undefined;
+
+    let {type, memberPath, finishedPath} = this.getPathInfo(workingUri, getPath);
+
+    try {
+      switch (type) {
+      case `member`:
+        if (this.copyBooks[finishedPath]) {
+          lines = this.copyBooks[finishedPath];
+        } else {
+          content = await contentApi.downloadMemberContent(memberPath[0], memberPath[1], memberPath[2], memberPath[3]);
+          lines = content.replace(new RegExp(`\\\r`, `g`), ``).split(`\n`);
+          this.copyBooks[finishedPath] = lines;
+        }
+        break;
+
+      case `streamfile`:
+        if (this.copyBooks[finishedPath]) {
+          lines = this.copyBooks[finishedPath];
+        } else {
+          content = await contentApi.downloadStreamfile(finishedPath);
+          lines = content.replace(new RegExp(`\\\r`, `g`), ``).split(`\n`);
+          this.copyBooks[finishedPath] = lines;
+        }
+        break;
+      }
+    } catch (e) {
+      lines = [];
+    }
+
+    return lines;
+  }
+
+  /**
+   * @param {vscode.Uri} workingUri
    * @param {string} content 
    */
-  getDocs(content) {
-    const lines = content.replace(new RegExp(`\\\r`, `g`), ``).split(`\n`);
+  async getDocs(workingUri, content) {
+    let lines = content.replace(new RegExp(`\\\r`, `g`), ``).split(`\n`);
     let currentComments = [], currentExample = [], currentItem, currentSub;
 
     let parts, partsLower, pieces;
@@ -99,6 +253,19 @@ module.exports = class RPGLinter {
     this.structs = [];
     this.procedures = [];
 
+    //First loop is for copy/include statements
+    for (let i = lines.length - 1; i >= 0; i--) {
+      let line = lines[i].trim(); //Paths are case insensitive so it's okay
+      if (line === ``) continue;
+
+      pieces = line.split(` `).filter(piece => piece !== ``);
+
+      if ([`/COPY`, `/INCLUDE`].includes(pieces[0].toUpperCase())) {
+        lines.splice(i, 1, ...(await this.getContent(workingUri, pieces[1])));
+      }
+    }
+
+    //Now the real work
     for (let line of lines) {
       line = line.trim();
 
@@ -144,6 +311,23 @@ module.exports = class RPGLinter {
         
       case `DCL-PR`:
         if (parts.find(element => element.startsWith(`EXTPROC`)) || parts.find(element => element.startsWith(`EXTPGM`))) {
+          if (!this.procedures.find(proc => proc.name.toUpperCase() === parts[1])) {
+            currentItem = new Declaration(`procedure`);
+            currentItem.name = partsLower[1];
+            currentItem.keywords = parts.slice(2);
+            currentItem.comments = currentComments.join(` `);
+            currentItem.example = currentExample;
+
+            currentComments = [];
+            currentExample = [];
+          }
+        } else {
+          console.log(`Procedures require EXTPROC or EXTPGM`);
+        }
+        break;
+
+      case `DCL-PI`:
+        if (!this.procedures.find(proc => proc.name.toUpperCase() === parts[1])) {
           currentItem = new Declaration(`procedure`);
           currentItem.name = partsLower[1];
           currentItem.keywords = parts.slice(2);
@@ -152,20 +336,7 @@ module.exports = class RPGLinter {
 
           currentComments = [];
           currentExample = [];
-        } else {
-          console.log(`Procedures require EXTPROC or EXTPGM`);
         }
-        break;
-
-      case `DCL-PI`:
-        currentItem = new Declaration(`procedure`);
-        currentItem.name = partsLower[1];
-        currentItem.keywords = parts.slice(2);
-        currentItem.comments = currentComments.join(` `);
-        currentItem.example = currentExample;
-
-        currentComments = [];
-        currentExample = [];
         break;
 
       case `END-PR`:
@@ -187,13 +358,16 @@ module.exports = class RPGLinter {
             currentExample.push(``);
           }
 
+        } else if (line.startsWith(`//`)) {
+          //Do nothing. Because it's a comment.
+
         } else {
           if (currentItem) {
             if (parts[0].startsWith(`DCL`))
               parts.slice(1);
 
             currentSub = new Declaration(`subitem`);
-            currentSub.name = partsLower[0];
+            currentSub.name = (parts[0] === `*N` ? `parm${currentItem.subItems.length+1}` : partsLower[0]) ;
             currentSub.keywords = parts.slice(1);
             currentSub.comments = currentComments.join(` `);
 
