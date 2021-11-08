7cd9fa8b1ef7c045c5cb58890a2c3b3f580935d3
Liam Barry Allan <liam@worksofbarry.com>
2021-11-08 16:27:34 -0500

    Correct name of prop when fetching source files

--- src/api/IBMiContent.js
+++ src/api/IBMiContent.js
@@ -204,7 +204,7 @@ module.exports = class IBMiContent {
       const results = await this.getTable(tempLib, TempName, TempName);
 
       if (results.length === 1) {
-        if (results[0].ODOBNM.trim() === ``) {
+        if (results[0].PHFILE.trim() === ``) {
           return []
         }
       }
