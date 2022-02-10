f104f1f4e9d7fad6e86c7dec0209adefb94b95d1
Liam Barry Allan <liam@worksofbarry.com>
2022-02-10 17:53:02 -0500

    Add missing throw

--- src/views/objectBrowser.js
+++ src/views/objectBrowser.js
@@ -563,6 +563,8 @@ module.exports = class objectBrowserTwoProvider {
               connection.sqlEnabled = false;
               this.refresh();
             }
+          } else {
+            throw e;
           }
         }
 
