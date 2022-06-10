467440850c7d6eb26e087ed5375a2584f3abe24a
Christian Jorgensen <cj@dk.nmp.eu>
2022-06-11 00:44:10 +0200

    Make sorting of IFS shortcuts case-insensitive

--- src/views/ifsBrowser.js
+++ src/views/ifsBrowser.js
@@ -119,7 +119,13 @@ module.exports = class ifsBrowserProvider {
 
         try {
 
-          shortcuts.sort();
+          shortcuts.sort(function(a, b){
+            let x = a.toLowerCase();
+            let y = b.toLowerCase();
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;
+          });
           await config.set(`ifsShortcuts`, shortcuts);
           if (Configuration.get(`autoRefresh`)) this.refresh();
         } catch (e) {
