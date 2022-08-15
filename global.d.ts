f8f030bd10a2ec7b92ea3dd24316a3d5f5db9d76
Liam Barry Allan <mrliamallan@live.co.uk>
2022-08-15 14:53:54 -0400

    New API for connecting to a system
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- global.d.ts
+++ global.d.ts
@@ -8,3 +8,13 @@ interface Action {
   deployFirst?: boolean;
   postDownload?: string[];
 }
+
+interface ConnectionData {
+  name: string;
+  host: string;
+  port: number;
+  username: string;
+  password?: string;
+  privateKey?: string;
+  keepaliveInterval: number;
+}
