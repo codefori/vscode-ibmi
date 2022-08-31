bc1942dc58c89bc2f419ae55e554b89e5cfea9a6
Liam Barry Allan <mrliamallan@live.co.uk>
2022-08-31 19:26:44 -0400

    Initial debug command
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

---
+++ src/api/Debug.js
@@ -0,0 +1,109 @@
+const vscode = require(`vscode`);
+const path = require(`path`);
+
+const Configuration = require(`./Configuration`);
+const IBMi = require(`./IBMi`);
+
+/**
+ * @param {*} instance 
+ * @param {vscode.ExtensionContext} context 
+ */
+exports.initialise = (instance, context) => {
+  const startDebugging = (options) => {
+    exports.startDebug(instance, options);
+  }
+
+  /** @param {vscode.Uri} uri */
+  const getObjectFromUri = (uri) => {
+    /** @type {IBMi} */
+    const connection = instance.getConnection();
+  
+    /** @type {Configuration} */
+    const configuration = instance.getConfig();
+    
+    const qualifiedPath = {
+      library: undefined,
+      object: undefined
+    };
+
+    switch (uri.scheme) {
+    case `member`:
+      const memberPath = connection.parserMemberPath(uri.path);
+      qualifiedPath.library = memberPath.library;
+      qualifiedPath.object = memberPath.member;
+      break;
+    case `streamfile`:
+    case `file`:
+      const parsedPath = path.parse(uri.path);
+      qualifiedPath.library = configuration.currentLibrary;
+      qualifiedPath.object = parsedPath.name;
+      break;
+    }
+
+    return qualifiedPath;
+  }
+
+  const getPassword = async () => {
+    /** @type {IBMi} */
+    const connection = instance.getConnection();
+
+    let password = await context.secrets.get(`${connection.currentConnectionName}_password`);
+    if (!password) {
+      password = await vscode.window.showInputBox({
+        password: true,
+        prompt: `Password for user profile ${connection.currentUser} is required to debug.`
+      });
+    }
+
+    return password;
+  }
+  
+  context.subscriptions.push(
+    vscode.commands.registerCommand(`code-for-ibmi.debug.activeEditor`, async () => {
+      const activeEditor = vscode.window.activeTextEditor;
+
+      if (activeEditor) {
+        const qualifiedObject = getObjectFromUri(activeEditor.document.uri);
+        const password = await getPassword();
+
+        if (password) {
+          startDebugging({
+            ...qualifiedObject,
+            password
+          });
+        }
+      }
+    })
+  )
+}
+
+/**
+ * @param {*} instance 
+ * @param {{password: string, library: string, object: string}} options
+ */
+exports.startDebug = async (instance, options) => {
+  /** @type {IBMi} */
+  const connection = instance.getConnection();
+  const port = `8005`; //TODO: make configurable
+  const updateProductionFiles = false;
+  const enableDebugTracing = true;
+
+  const config = {
+    "type": `IBMiDebug`,
+    "request": `launch`,
+    "name": `Remote debug: Launch a batch debug session`,
+    "user": connection.currentUser.toUpperCase(),
+    "password": options.password,
+    "host": connection.currentHost,
+    "port": port,
+    "secure": true,  // Enforce secure mode
+    "ignoreCertificateErrors": false,
+    "library": options.library.toUpperCase(),
+    "program": options.object.toUpperCase(),
+    "startBatchJobCommand": `SBMJOB CMD(CALL PGM(` + options.library + `/` + options.object + `))`,
+    "updateProductionFiles": updateProductionFiles,
+    "trace": enableDebugTracing,
+  };
+
+  vscode.debug.startDebugging(undefined, config, undefined);
+}
