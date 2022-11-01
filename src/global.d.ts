7fce08db5a4ba07d95a81b0306b085ea8909fb1e
Seb Julliand <sebjulliand@gmail.com>
2022-11-01 19:21:23 +0100

    Made Action's type optional

--- src/global.d.ts
+++ src/global.d.ts
@@ -29,7 +29,7 @@ interface MemberParts {
 interface Action {
   name: string;
   command: string;
-  type: "member"|"streamfile"|"object"|"file";
+  type?: "member"|"streamfile"|"object"|"file";
   environment: "ile"|"qsh"|"pase";
   extensions: string[];
   deployFirst?: boolean;
