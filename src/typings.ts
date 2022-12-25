f96b957809d80dce21f5c48e3af59958cf4d4389
Seb Julliand <sebjulliand@gmail.com>
2022-12-25 18:14:44 +0100

    Export deploy function

--- src/typings.ts
+++ src/typings.ts
@@ -9,6 +9,7 @@ export interface CodeForIBMi {
   Field: object //Field: typeof Field;
   deploy: (parameters: DeploymentParameters) => Promise<boolean>
   evfeventParser: (lines: string[]) => Map<string, FileError[]>
+  deploy: (parameters: Deployment.DeploymentParameters) => Promise<boolean>
 }
 
 export enum DeploymentMethod {
