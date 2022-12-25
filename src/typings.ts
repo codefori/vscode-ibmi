8129b80796f15bb218d944b234a1577c98a0864c
Seb Julliand <sebjulliand@gmail.com>
2022-12-25 18:14:44 +0100

    Export deploy function

--- src/typings.ts
+++ src/typings.ts
@@ -9,6 +9,7 @@ export interface CodeForIBMi {
   Field: object //Field: typeof Field;
   deploy: (parameters: Deployment.DeploymentParameters) => Promise<boolean>
   evfeventParser: (lines: string[]) => Map<string, FileError[]>
+  deploy: (parameters: Deployment.DeploymentParameters) => Promise<boolean>
 }
 
 export interface StandardIO {
