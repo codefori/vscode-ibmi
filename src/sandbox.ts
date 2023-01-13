53ce8670df00d5fc717c05e17c54cd2d6a636425
Liam Barry Allan <mrliamallan@live.co.uk>
2023-01-13 13:47:40 -0500

    Sandbox mode setup
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,16 +1,83 @@
 import { env } from "process";
-import { extensions } from "vscode";
+import { commands, extensions, window } from "vscode";
+import { ConnectionConfiguration } from "./api/Configuration";
 import { GitExtension } from "./api/import/git";
+import { instance } from "./instantiate";
+import { ConnectionData } from "./typings";
 
-export default function () {
+export default async function () {
   if (env.VSCODE_IBMI_SANDBOX && env.SANDBOX_SERVER) {
     console.log(`Sandbox mode enabled. Look at branch name as username`);
     const gitAPI = extensions.getExtension<GitExtension>(`vscode.git`)?.exports.getAPI(1);
     if (gitAPI && gitAPI.repositories && gitAPI.repositories.length > 0) {
       const repo = gitAPI.repositories[0];
-      const username = repo.state.HEAD?.name;
+      const branchName = repo.state.HEAD?.name;
 
-      console.log(`${env.SANDBOX_SERVER}@${username}:${username}`);
+      if (branchName) {
+        console.log(`${env.SANDBOX_SERVER}@${branchName}:${branchName}`);
+
+        const username = branchName.toUpperCase();
+
+        const connectionData: ConnectionData = {
+          host: env.SANDBOX_SERVER,
+          name: `Sandbox-${username}`,
+          username: username,
+          password: username,
+          port: 22,
+          privateKey: null,
+          keepaliveInterval: 35000
+        };
+
+        window.showInformationMessage(`Thanks for trying the Code for IBM i Sandbox!`, {
+          modal: true,
+          detail: `You are using this system at your own risk. Do not share any sensitive or private information.`
+        });
+        
+        const connectionResult = await commands.executeCommand(`code-for-ibmi.connectDirect`, connectionData);
+
+        if (connectionResult) {
+          const config = instance.getConfig();
+          if (config) {
+            const libraryList = config.libraryList;
+            if (!libraryList.includes(username)) {
+              config.libraryList = [...config.libraryList, username];
+
+              config.objectFilters.push(
+                {
+                  name: "Sandbox Sources",
+                  library: username,
+                  object: "*",
+                  types: [
+                    "*SRCPF"
+                  ],
+                  member: "*",
+                  memberType: ""
+                },
+                {
+                  name: "Sandbox Object Filters",
+                  library: username,
+                  object: "*",
+                  types: [
+                    "*ALL"
+                  ],
+                  member: "*",
+                  memberType: ""
+                },
+              );
+
+              await ConnectionConfiguration.update(config);
+              commands.executeCommand(`code-for-ibmi.refreshLibraryListView`);
+              commands.executeCommand(`code-for-ibmi.refreshObjectBrowser`);
+            }
+          }
+
+        } else {
+          window.showInformationMessage(`Ohno! The sandbox is down.`, {
+            modal: true,
+            detail: `Sorry, but the sandbox is offline right now. Try again another time.`
+          });
+        }
+      }
     }
   }
-}
+} 
