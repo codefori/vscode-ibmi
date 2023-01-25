46cabfd94d31688b60b2c60e3b755df3e01530bb
Liam Barry Allan <mrliamallan@live.co.uk>
2023-01-24 23:11:26 -0500

    Better user message if already connected
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -12,82 +12,91 @@ export async function registerUriHandler(context: ExtensionContext) {
       async handleUri(uri: Uri) {
         console.log(uri);
 
+        const connection = instance.getConnection();
+
         switch (uri.path) {
           case `/connect`:
-            const queryData = querystring.parse(uri.query);
+            if (connection === undefined) {
+              const queryData = querystring.parse(uri.query);
 
-            const save = queryData.save === `true`;
-            const server = queryData.server;
-            let user: string | string[] | undefined = queryData.user;
-            let pass: string | string[] | undefined = queryData.pass;
+              const save = queryData.save === `true`;
+              const server = queryData.server;
+              let user: string | string[] | undefined = queryData.user;
+              let pass: string | string[] | undefined = queryData.pass;
 
-            if (server) {
-              if (!user) {
-                user = await window.showInputBox({
-                  title: `User for server`,
-                  prompt: `Enter username for ${server}`
-                });
-              }
-
-              if (pass) {
-                pass = Buffer.from(String(pass), `base64`).toString();
-              } else {
-                pass = await window.showInputBox({
-                  password: true,
-                  title: `Password for server`,
-                  prompt: `Enter password for ${user}@${server}`
-                });
-              }
-
-              if (user && pass) {
-                const serverParts = String(server).split(`:`);
-                const host = serverParts[0];
-                const port = serverParts.length === 2 ? Number(serverParts[1]) : 22;
-
-                const connectionData: ConnectionData = {
-                  host,
-                  name: `${user}-${host}`,
-                  username: String(user),
-                  password: String(pass),
-                  port,
-                  privateKey: null
-                };
-
-                const connectionResult = await commands.executeCommand(`code-for-ibmi.connectDirect`, connectionData);
-
-                if (connectionResult) {
-                  await initialSetup(connectionData.username);
-
-                  if (save) {
-                    let existingConnections: ConnectionData[]|undefined = GlobalConfiguration.get(`connections`);
-
-                    if (existingConnections) {
-                      const existingConnection = existingConnections.find(item => item.name === host);
-
-                      if (!existingConnection) {
-                        // New connection!
-                        existingConnections.push({
-                          ...connectionData,
-                          password: undefined, // Removes the password from the object
-                        });
-
-                        context.secrets.store(`${host}_password`, pass);
-
-                        await GlobalConfiguration.set(`connections`, existingConnections);
-                      }
-                    }
-                  }
-
-                } else {
-                  window.showInformationMessage(`Failed to connect`, {
-                    modal: true,
-                    detail: `Failed to connect to ${server} as ${user}.`
+              if (server) {
+                if (!user) {
+                  user = await window.showInputBox({
+                    title: `User for server`,
+                    prompt: `Enter username for ${server}`
                   });
                 }
 
-              } else {
-                window.showErrorMessage(`Connection to ${server} ended as no password was provided.`);
+                if (pass) {
+                  pass = Buffer.from(String(pass), `base64`).toString();
+                } else {
+                  pass = await window.showInputBox({
+                    password: true,
+                    title: `Password for server`,
+                    prompt: `Enter password for ${user}@${server}`
+                  });
+                }
+
+                if (user && pass) {
+                  const serverParts = String(server).split(`:`);
+                  const host = serverParts[0];
+                  const port = serverParts.length === 2 ? Number(serverParts[1]) : 22;
+
+                  const connectionData: ConnectionData = {
+                    host,
+                    name: `${user}-${host}`,
+                    username: String(user),
+                    password: String(pass),
+                    port,
+                    privateKey: null
+                  };
+
+                  const connectionResult = await commands.executeCommand(`code-for-ibmi.connectDirect`, connectionData);
+
+                  if (connectionResult) {
+                    await initialSetup(connectionData.username);
+
+                    if (save) {
+                      let existingConnections: ConnectionData[] | undefined = GlobalConfiguration.get(`connections`);
+
+                      if (existingConnections) {
+                        const existingConnection = existingConnections.find(item => item.name === host);
+
+                        if (!existingConnection) {
+                          // New connection!
+                          existingConnections.push({
+                            ...connectionData,
+                            password: undefined, // Removes the password from the object
+                          });
+
+                          context.secrets.store(`${host}_password`, pass);
+
+                          await GlobalConfiguration.set(`connections`, existingConnections);
+                        }
+                      }
+                    }
+
+                  } else {
+                    window.showInformationMessage(`Failed to connect`, {
+                      modal: true,
+                      detail: `Failed to connect to ${server} as ${user}.`
+                    });
+                  }
+
+                } else {
+                  window.showErrorMessage(`Connection to ${server} ended as no password was provided.`);
+                }
               }
+            } else {
+              window.showInformationMessage(`Failed to connect`, {
+                modal: true,
+                detail: `This Visual Studio Code instance is already connected to a server.`
+              });
             }
 
             break;
@@ -163,7 +172,7 @@ export async function handleStartup() {
       });
     }
   }
-} 
+}
 
 async function initialSetup(username: string) {
   const config = instance.getConfig();
