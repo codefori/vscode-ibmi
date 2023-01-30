45aa6e5468922ac756f5011c9755aff5bfd80d20
BOURREAU Sébastien <sebastien.bourreau@cooperl.com>
2023-01-30 13:06:23 +0100

    return an error instead of an object

--- src/views/ifsBrowser.js
+++ src/views/ifsBrowser.js
@@ -554,21 +554,16 @@ module.exports = class ifsBrowserProvider {
         try {
           const objects = await content.getFileList(element.path);
 
-          if (objects.code && objects.code > 0){
-            vscode.window.showErrorMessage(objects.text);
-          } else {
+          items = objects.filter(o => o.type === `directory`)
+            .concat(objects.filter(o => o.type === `streamfile`))
+            .map(object => new Object(object.type, object.name, object.path));
 
-            items = objects.filter(o => o.type === `directory`)
-              .concat(objects.filter(o => o.type === `streamfile`))
-              .map(object => new Object(object.type, object.name, object.path));
-
-            await this.storeIFSList(element.path, objects.filter(o => o.type === `streamfile`).map(o => o.name));
-          }
+          await this.storeIFSList(element.path, objects.filter(o => o.type === `streamfile`).map(o => o.name));
 
         } catch (e) {
           console.log(e);
           item = new vscode.TreeItem(`Error loading objects.`);
-          vscode.window.showErrorMessage(e);
+          vscode.window.showErrorMessage(e.message);
           items = [item];
         }
 
