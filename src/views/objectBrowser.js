b8625a59aa853ba16bd4541a7e4b5c617aa77c37
Seb Julliand <sebjulliand@gmail.com>
2023-03-08 19:30:28 +0100

    Prevent search's member filter from being empty

--- src/views/objectBrowser.js
+++ src/views/objectBrowser.js
@@ -552,7 +552,7 @@ module.exports = class objectBrowserTwoProvider {
                       }
                     }, timeoutInternal);
 
-                    let results = await Search.searchMembers(instance, path[0], path[1], `${node.memberFilter}.MBR`, searchTerm);
+                    let results = await Search.searchMembers(instance, path[0], path[1], `${node.memberFilter || `*`}.MBR`, searchTerm);
 
                     // Filter search result by member type filter.
                     if (results.length > 0 && node.memberTypeFilter) {
