87c0d9084bbcbdc6c84fcbaee0e3cc81b6dbe0a0
Liam Allan <mrliamallan@live.co.uk>
2023-03-09 12:58:00 -0500

    Libl tests
    Signed-off-by: Liam Allan <mrliamallan@live.co.uk>

--- src/api/CompileTools.ts
+++ src/api/CompileTools.ts
@@ -570,8 +570,9 @@ export namespace CompileTools {
               command: [
                 ...buildLiblistCommands(connection, config),
                 ...commands.map(command =>
-                  `${`system ${GlobalConfiguration.get(`logCompileOutput`) ? `` : `-s`} "${command.replace(/[$]/g, `\\$&`)}"; if [[ $? -ne 0 ]]; then exit 1; fi`}`
+                  `${`system ${GlobalConfiguration.get(`logCompileOutput`) ? `` : `-s`} "${command.replace(/[$]/g, `\\$&`)}"; if [[ $? -ne 0 ]]; then exit 1; fi`}`,
                 ),
+                `liblist`
               ].join(` && `),
               directory: cwd,
               ...callbacks
