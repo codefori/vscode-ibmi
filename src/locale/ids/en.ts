c44ac728c07ec1d56d2f7cc5f7f0622bf5b57b6b
Liam Allan <mrliamallan@live.co.uk>
2023-03-19 12:41:34 -0400

    Initial work for l10n
    Signed-off-by: Liam Allan <mrliamallan@live.co.uk>

---
+++ src/locale/ids/en.ts
@@ -0,0 +1,18 @@
+import { Locale } from "..";
+
+export const en: Locale = {
+  'Yes': `Yes`,
+  'No': `No`,
+  'sandbox.input.user.title': `User for server`,
+  'sandbox.input.user.prompt': `Enter username for {0}`,
+  'sandbox.input.password.title': `Password for server`,
+  'sandbox.input.password.prompt': `Enter password for {0}@{1}`,
+  'sandbox.failedToConnect.text': 'Failed to connect to {0} as {1}',
+  'sandbox.failedToConnect.title': `Failed to connect`,
+  'sandbox.noPassword': `Connection to {0} ended as no password was provided.`,
+  'sandbox.alreadyConnected': `This Visual Studio Code instance is already connected to a server.`,
+  'sandbox.connected.modal.title': `Thanks for trying the Code for IBM i Sandbox!`,
+  'sandbox.connected.modal.detail': `You are using this system at your own risk. Do not share any sensitive or private information.`,
+  'sandbox.noconnection.modal.title': `Oh no! The sandbox is down.`,
+  'sandbox.noconnection.modal.detail': `Sorry, but the sandbox is offline right now. Try again another time.`
+};
---
+++ src/locale/index.ts
@@ -0,0 +1,23 @@
+import { env } from "vscode";
+import {en} from "./ids/en";
+
+const locales: {[locale: string]: Locale} = {
+  en
+}
+
+export type Locale = {[id: string]: string};
+
+export function t(id: string, values: string[] = []) {
+  const currentLocal = env.language;
+
+  // Check for the id in their local local first, then default to en, then just show the id.
+  let text = locales[currentLocal] ? locales[currentLocal][id] : (locales.en[id] || `!${id}!`);
+
+  if (values.length > 0) {
+    values.forEach((value, i) => {
+      text = text.replace(new RegExp(`{${i}}`, `g`), value);
+    });
+  }
+
+  return text;
+}
