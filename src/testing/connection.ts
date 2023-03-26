a14c4599799f4c546a4be015c36d3f5232057a12
Liam Allan <mrliamallan@live.co.uk>
2023-03-25 22:25:58 -0400

    Additional tests for member parsing
    Signed-off-by: Liam Allan <mrliamallan@live.co.uk>

--- src/testing/connection.ts
+++ src/testing/connection.ts
@@ -56,4 +56,74 @@ export const ConnectionSuite: TestSuite = [
     assert.strictEqual(result?.code, 0);
     assert.strictEqual(result?.stdout, `Hello world cool`);
   }},
+
+  {name: `Test getTempRemote`, test: async () => {
+    const connection = instance.getConnection();
+
+    const fileA = connection?.getTempRemote(`/some/file`);
+    const fileB = connection?.getTempRemote(`/some/badfile`);
+    const fileC = connection?.getTempRemote(`/some/file`);
+
+    assert.strictEqual(fileA, fileC);
+    assert.notStrictEqual(fileA, fileB);
+  }},
+
+  {name: `Test parserMemberPath (simple)`, test: async () => {
+    const connection = instance.getConnection();
+
+    const memberA = connection?.parserMemberPath(`/thelib/thespf/thembr.mbr`);
+
+    assert.strictEqual(memberA?.asp, undefined);
+    assert.strictEqual(memberA?.library, `THELIB`);
+    assert.strictEqual(memberA?.file, `THESPF`);
+    assert.strictEqual(memberA?.member, `THEMBR`);
+    assert.strictEqual(memberA?.extension, `MBR`);
+    assert.strictEqual(memberA?.basename, `THEMBR.MBR`);
+  }},
+
+  {name: `Test parserMemberPath (ASP)`, test: async () => {
+    const connection = instance.getConnection();
+
+    const memberA = connection?.parserMemberPath(`/theasp/thelib/thespf/thembr.mbr`);
+
+    assert.strictEqual(memberA?.asp, `THEASP`);
+    assert.strictEqual(memberA?.library, `THELIB`);
+    assert.strictEqual(memberA?.file, `THESPF`);
+    assert.strictEqual(memberA?.member, `THEMBR`);
+    assert.strictEqual(memberA?.extension, `MBR`);
+    assert.strictEqual(memberA?.basename, `THEMBR.MBR`);
+  }},
+
+  {name: `Test parserMemberPath (no root)`, test: async () => {
+    const connection = instance.getConnection();
+
+    const memberA = connection?.parserMemberPath(`thelib/thespf/thembr.mbr`);
+
+    assert.strictEqual(memberA?.asp, undefined);
+    assert.strictEqual(memberA?.library, `THELIB`);
+    assert.strictEqual(memberA?.file, `THESPF`);
+    assert.strictEqual(memberA?.member, `THEMBR`);
+    assert.strictEqual(memberA?.extension, `MBR`);
+    assert.strictEqual(memberA?.basename, `THEMBR.MBR`);
+  }},
+
+  {name: `Test parserMemberPath (no extension)`, test: async () => {
+    const connection = instance.getConnection();
+
+    try {
+      const memberA = connection?.parserMemberPath(`thelib/thespf/thembr`);
+    } catch (e: any) {
+      assert.strictEqual(e.message, `Source Type extension is required.`);
+    }
+  }},
+
+  {name: `Test parserMemberPath (invalid length)`, test: async () => {
+    const connection = instance.getConnection();
+
+    try {
+      const memberA = connection?.parserMemberPath(`/thespf/thembr.mbr`);
+    } catch (e: any) {
+      assert.strictEqual(e.message, `Invalid path: /thespf/thembr.mbr. Use format LIB/SPF/NAME.ext`);
+    }
+  }},
 ];
