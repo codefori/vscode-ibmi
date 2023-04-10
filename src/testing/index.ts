629fb0b385099eacae8cd324338976438f8ea78c
Seb Julliand <sebjulliand@gmail.com>
2023-04-10 11:03:55 +0200

    Simple test UI

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,21 +1,32 @@
+import vscode from "vscode";
 import { env } from "process";
 import { instance } from "../instantiate";
 import { ConnectionSuite } from "./connection";
+import { TestSuitesTreeProvider } from "./testCasesTree";
 
-const suites = [
-  {name: `Connection tests`, tests: ConnectionSuite}
+const suites : TestSuite[] = [
+  ConnectionSuite
 ]
 
-export type TestSuite = TestCase[];
+export type TestSuite = {
+  name: string
+  tests: TestCase[]
+}
 
 export interface TestCase {
   name: string,
+  status?: "running" | "failed" | "pass"
+  failure?: string
   test: () => Promise<void>
 }
 
-export function initialise() {
+let testSuitesTreeProvider : TestSuitesTreeProvider;
+export function initialise(context: vscode.ExtensionContext) {
   if (env.testing === `true`) {
+    vscode.commands.executeCommand(`setContext`, `code-for-ibmi:testing`, true);
     instance.onEvent(`connected`, runTests);
+    testSuitesTreeProvider = new TestSuitesTreeProvider(suites);
+    context.subscriptions.push(vscode.window.registerTreeDataProvider("testingView", testSuitesTreeProvider));
   }
 }
 
@@ -23,9 +34,22 @@ async function runTests() {
   for (const suite of suites) {
     console.log(`Running suite ${suite.name} (${suite.tests.length})`);
     console.log();
-    for (const test of suite.tests) {
+    for (const test of suite.tests) {      
       console.log(`\tRunning ${test.name}`);
-      await test.test();
+      test.status = "running";
+      testSuitesTreeProvider.refresh();
+      try{
+        await test.test();
+        test.status = "pass";
+      }
+      catch(error: any){
+        console.log(error);
+        test.status = "failed";
+        test.failure = error.message;
+      }
+      finally{
+        testSuitesTreeProvider.refresh();
+      }
     }
   }
 }
