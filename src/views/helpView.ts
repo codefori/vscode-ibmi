7bd05ec165a8e2b93f25534c52601d860d4d6d2a
Seb Julliand <sebjulliand@gmail.com>
2023-05-28 19:13:16 +0200

    Always display help view
    Issue content now depends on the connection's state

--- src/views/helpView.ts
+++ src/views/helpView.ts
@@ -1,69 +1,97 @@
 
 import vscode from 'vscode';
-import {instance} from '../instantiate';
+import { instance } from '../instantiate';
 
 export class HelpView {
-  private _extension_version: string;
 
-  constructor(context: vscode.ExtensionContext) {
-    this._extension_version = context.extension.packageJSON.version;
+  constructor() {
+    vscode.commands.registerCommand("code-for-ibmi.openNewIssue", openNewIssue)
   }
 
-  public getTreeItem(element : vscode.TreeItem) : vscode.TreeItem {
+  public getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
     return element;
   }
 
-  public async getChildren() : Promise<HelpItem[]>{
-    const connection = instance.getConnection();
-    const config = instance.getConfig();
-
-    const issueUrl = [
-      `Issue text goes here.`,
-      ``,
-      `Code for IBM i version: ${this._extension_version}`,
-      `${vscode.env.appName} version: ${vscode.version}`,
-      `Platform: ${process.platform}`,
-      ``,
-      `* QCCSID: ${connection?.qccsid || '?'}`,
-      `* Features:`,
-      ...Object.keys(connection?.remoteFeatures || {}).map(
-        (feature) => `   * ${feature}: ${connection?.remoteFeatures[feature] !== undefined}`
-      ),
-      `* SQL enabled: ${config ? config.enableSQL : '?'}`,
-      `* Source dates enabled: ${config ? config.enableSourceDates : '?'}`,
-      ``,
-      `Variants`,
-      `\`\`\`json`,
-      JSON.stringify(connection?.variantChars || {}, null, 2),
-      `\`\`\``,
-      ``,
-      `Errors:`,
-      `\`\`\`json`,
-      JSON.stringify(connection?.lastErrors || [], null, 2),
-      `\`\`\``,
-    ].join(`\n`);
-
+  public async getChildren(): Promise<HelpItem[]> {
     return [
-      new HelpItem(`book`, `Get started`, `https://halcyon-tech.github.io/vscode-ibmi/#/`),
-      new HelpItem(`output`, `Open official Forum`, `https://github.com/halcyon-tech/vscode-ibmi/discussions`),
-      new HelpItem(`eye`, `Review Issues`, `https://github.com/halcyon-tech/vscode-ibmi/issues/`),
-      new HelpItem(`bug`, `Report an Issue`, `https://github.com/halcyon-tech/vscode-ibmi/issues/new?body=${encodeURIComponent(issueUrl)}`),
+      new HelpOpenUrkItem(`book`, `Get started`, `https://halcyon-tech.github.io/vscode-ibmi/#/`),
+      new HelpOpenUrkItem(`output`, `Open official Forum`, `https://github.com/halcyon-tech/vscode-ibmi/discussions`),
+      new HelpOpenUrkItem(`eye`, `Review Issues`, `https://github.com/halcyon-tech/vscode-ibmi/issues/`),
+      new HelpIssueItem(),
     ];
   }
 }
 
 class HelpItem extends vscode.TreeItem {
-  constructor(icon : string, text: string, url: string) {
+  constructor(icon: string, readonly text: string) {
     super(text, vscode.TreeItemCollapsibleState.None);
 
-    this.contextValue = `hitSource`;
-    
+    this.contextValue = `helpItem`;
     this.iconPath = new vscode.ThemeIcon(icon);
+  }
+}
+
+class HelpOpenUrkItem extends HelpItem {
+  constructor(icon: string, text: string, url: string) {
+    super(icon, text);
 
     this.command = {
       command: `vscode.open`,
       title: text,
       arguments: [vscode.Uri.parse(url)]
     };
+  }
+}
+
+class HelpIssueItem extends HelpItem {
+  constructor() {
+    super(`bug`, `Report an Issue`);
+
+    this.command = {
+      command: "code-for-ibmi.openNewIssue",
+      title: this.text
+    };
+  }
+}
+
+function openNewIssue() {
+  const code4ibmi = vscode.extensions.getExtension("halcyontechltd.code-for-ibmi");
+  const issueUrl = [
+    `Issue text goes here.`,
+    ``,
+    `Code for IBM i version: ${code4ibmi?.packageJSON.version}`,
+    `${vscode.env.appName} version: ${vscode.version}`,
+    `Platform: ${process.platform}`,
+    ``,
+    getRemoteSection(),
+  ].join(`\n`);
+
+  vscode.commands.executeCommand(`vscode.open`, `https://github.com/halcyon-tech/vscode-ibmi/issues/new?body=${encodeURIComponent(issueUrl)}`);
+}
+
+function getRemoteSection() {
+  const connection = instance.getConnection();
+  const config = instance.getConfig();
+  if (connection && config) {
+    return [`* QCCSID: ${connection?.qccsid || '?'}`,
+      `* Features:`,
+    ...Object.keys(connection?.remoteFeatures || {}).map(
+      (feature) => `   * ${feature}: ${connection?.remoteFeatures[feature] !== undefined}`
+    ),
+    `* SQL enabled: ${config ? config.enableSQL : '?'}`,
+    `* Source dates enabled: ${config ? config.enableSourceDates : '?'}`,
+      ``,
+      `Variants`,
+      `\`\`\`json`,
+    JSON.stringify(connection?.variantChars || {}, null, 2),
+      `\`\`\``,
+      ``,
+      `Errors:`,
+      `\`\`\`json`,
+    JSON.stringify(connection?.lastErrors || [], null, 2),
+      `\`\`\``].join("\n");
+  }
+  else {
+    return "*_Not connected_*";
   }
 }
