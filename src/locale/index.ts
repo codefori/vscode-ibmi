315c740ce70a40441a6069a0145b79b71067fccc
Christian Jorgensen <chr.jorgensen1@gmail.com>
2023-07-09 22:28:36 +0200

    Fix check for local locale id

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -13,8 +13,8 @@ export type Locale = {[id: string]: string};
 const currentLocale = String(GlobalConfiguration.get(`locale`) || env.language);
 
 export function t(id: string, values: string[] = []) {
-  // Check for the id in their local local first, then default to en, then just show the id.
-  let text = locales[currentLocale] ? locales[currentLocale][id] : (locales.en[id] || `!${id}!`);
+  // Check for the id in their local locale first, then default to en, then just show the id.
+  let text = locales[currentLocale][id] ? locales[currentLocale][id] : (locales.en[id] || `!${id}!`);
 
   if (values.length > 0) {
     values.forEach((value, i) => {
