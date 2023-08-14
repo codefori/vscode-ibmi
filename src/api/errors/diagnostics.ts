66fadcd6471be179a834a54d2c0d737941311ff8
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-08-14 14:37:47 -0400

    fix
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/api/errors/diagnostics.ts
+++ src/api/errors/diagnostics.ts
@@ -3,6 +3,7 @@ import * as vscode from "vscode";
 import Instance from "../Instance";
 import { parseErrors } from "./handler";
 import { FileError } from "../../typings";
+import { getEvfeventFiles } from "../local/actions";
 
 const ileDiagnostics = vscode.languages.createDiagnosticCollection(`ILE`);
 
@@ -133,17 +134,6 @@ export async function handleEvfeventLines(lines: string[], instance: Instance, e
   } else {
     ileDiagnostics.clear();
   }
-}
-
-export async function getEvfeventFiles(currentWorkspace: vscode.WorkspaceFolder) {
-  const evfeventFiles: vscode.Uri[] = [];
-
-  if (currentWorkspace) {
-    const relativeSearch = new vscode.RelativePattern(currentWorkspace, `**/.evfevent/*`);
-    evfeventFiles.push(...await vscode.workspace.findFiles(relativeSearch));
-  }
-
-  return evfeventFiles;
 }
 
 const diagnosticSeverity = (error: FileError) => {
