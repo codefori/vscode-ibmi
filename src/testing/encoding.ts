86ea4d9fa645d54699ebf7001a2678ee03ad01f8
worksofliam <mrliamallan@live.co.uk>
2024-04-11 09:51:05 -0400

    Additional encoding tests
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/testing/encoding.ts
+++ src/testing/encoding.ts
@@ -8,70 +8,48 @@ import { instance } from "../instantiate";
 import { CommandResult } from "../typings";
 import { getMemberUri } from "../filesystems/qsys/QSysFs";
 
+const contents = {
+  '37': [`Hello world`],
+  '273': [`Hello world`, `àáãÄÜö£øß`],
+  '277': [`Hello world`, `çñßØ¢åæ`],
+  '297': [`Hello world`, `âÑéè¥ýÝÞã`],
+  '290': [`Hello world`, `ｦｯ!ﾓﾄｴﾜﾈﾁｾ`],
+  '420': [`Hello world`, `ص ث ب ﻷ`],
+}
+
 export const EncodingSuite: TestSuite = {
   name: `Encoding tests`,
   before: async () => {
     const config = instance.getConfig()!;
     assert.ok(config.enableSourceDates, `Source dates must be enabled for this test.`);
   },
+  tests:
+    Object.keys(contents).map(ccsid => {
+      return {
+        name: `Encoding ${ccsid}`, test: async () => {
+          const connection = instance.getConnection();
+          const config = instance.getConfig()!;
 
-  tests: [
-    {
-      name: `Encoding 37`, test: async () => {
-        const connection = instance.getConnection();
-        const config = instance.getConfig()!;
+          const lines = contents[ccsid as keyof typeof contents].join(`\n`);
 
-        const lines = [
-          `Hello world`
-        ].join(`\n`);
+          const tempLib = config!.tempLibrary;
 
-        const tempLib = config!.tempLibrary;
+          const file = `TEST${ccsid}`;
 
-        const file = `TEST37`;
+          await connection!.runCommand({ command: `CRTSRCPF FILE(${tempLib}/${file}) RCDLEN(112) CCSID(${ccsid})`, noLibList: true });
+          await connection!.runCommand({ command: `ADDPFM FILE(${tempLib}/${file}) MBR(THEMEMBER) SRCTYPE(TXT)`, noLibList: true });
 
-        await connection!.runCommand({ command: `CRTSRCPF FILE(${tempLib}/${file}) RCDLEN(112) CCSID(37)`, noLibList: true });
-        await connection!.runCommand({ command: `ADDPFM FILE(${tempLib}/${file}) MBR(THEMEMBER) SRCTYPE(TXT)`, noLibList: true });
+          const theBadOneUri = getMemberUri({ library: tempLib, file, name: `THEMEMBER`, extension: `TXT` });
 
-        const theBadOneUri = getMemberUri({library: tempLib, file, name: `THEMEMBER`, extension: `TXT`});
+          await workspace.fs.readFile(theBadOneUri);
 
-        await workspace.fs.readFile(theBadOneUri);
+          await workspace.fs.writeFile(theBadOneUri, Buffer.from(lines, `utf8`));
 
-        await workspace.fs.writeFile(theBadOneUri, Buffer.from(lines, `utf8`));
-
-        const memberContentBuf = await workspace.fs.readFile(theBadOneUri);
-        const fileContent = new TextDecoder().decode(memberContentBuf)
-        
-        assert.strictEqual(fileContent, lines);
-      },
-    },
-    {
-      name: `Encoding 273`, test: async () => {
-        const connection = instance.getConnection();
-        const config = instance.getConfig()!;
-
-        const lines = [
-          `Hello world`,
-          `àáãÄÜö£øß`
-        ].join(`\n`);
-
-        const tempLib = config!.tempLibrary;
-
-        const file = `TEST273`;
-
-        await connection!.runCommand({ command: `CRTSRCPF FILE(${tempLib}/${file}) RCDLEN(112) CCSID(273)`, noLibList: true });
-        await connection!.runCommand({ command: `ADDPFM FILE(${tempLib}/${file}) MBR(THEMEMBER) SRCTYPE(TXT)`, noLibList: true });
-
-        const theBadOneUri = getMemberUri({library: tempLib, file, name: `THEMEMBER`, extension: `TXT`});
-
-        await workspace.fs.readFile(theBadOneUri);
-
-        await workspace.fs.writeFile(theBadOneUri, Buffer.from(lines, `utf8`));
-
-        const memberContentBuf = await workspace.fs.readFile(theBadOneUri);
-        const fileContent = new TextDecoder().decode(memberContentBuf)
-        
-        assert.strictEqual(fileContent, lines);
+          const memberContentBuf = await workspace.fs.readFile(theBadOneUri);
+          const fileContent = new TextDecoder().decode(memberContentBuf)
+          
+          assert.strictEqual(fileContent, lines);
+        },
       }
-    }
-  ]
+    })
 };
