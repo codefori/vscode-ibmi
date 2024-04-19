fa6c828b61ac188c206a19d09221d170fec30ffc
worksofliam <mrliamallan@live.co.uk>
2024-04-19 10:23:03 -0400

    Change to DBCS byte length calculation
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/api/Tools.ts
+++ src/api/Tools.ts
@@ -107,7 +107,23 @@ export namespace Tools {
           const fromPos = header.from - slideBytesBy;
           let strValue = line.substring(fromPos, fromPos + header.length);
 
-          const extendedBytes = strValue.split(``).map(c => Buffer.byteLength(c) === 1 ? 0 : 1).reduce((a: number, b: number) => a + b, 0);
+          /* For each DBCS character, add 1
+          Since we are reading characters as UTF8 here, we assume any UTF8 character made up of more than 2 bytes is DBCS
+
+          https://stackoverflow.com/a/14495321/4763757
+
+          Look at a list of Unicode blocks and their code point ranges, e.g. 
+          the browsable http://www.fileformat.info/info/unicode/block/index.htm or 
+          the official http://www.unicode.org/Public/UNIDATA/Blocks.txt :
+
+          Anything up to U+007F takes 1 byte: Basic Latin
+          Then up to U+07FF it takes 2 bytes: Greek, Arabic, Cyrillic, Hebrew, etc
+          Then up to U+FFFF it takes 3 bytes: Chinese, Japanese, Korean, Devanagari, etc
+          Beyond that it takes 4 bytes
+
+          */
+
+          const extendedBytes = strValue.split(``).map(c => Buffer.byteLength(c) < 3 ? 0 : 1).reduce((a: number, b: number) => a + b, 0);
 
           slideBytesBy += extendedBytes;
           if (extendedBytes > 0) {
