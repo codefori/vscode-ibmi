6709a211532b0e673b75025478e367f83d0ae28e
Seb Julliand <sebjulliand@gmail.com>
2024-06-07 22:02:52 +0200

    Better where clause for getting debug service ports
    Signed-off-by: Seb Julliand <sebjulliand@gmail.com>

--- src/api/debug/server.ts
+++ src/api/debug/server.ts
@@ -112,7 +112,7 @@ export async function stopService(connection: IBMi) {
 export async function getDebugServiceJob() {
   const connection = instance.getConnection();
   if (connection) {
-    const rows = await connection.runSQL(`select distinct job_name, local_port from qsys2.netstat_job_info j where job_name = (select job_name from qsys2.netstat_job_info j where local_port = ${connection.config?.debugPort || 8005} and remote_address = '0.0.0.0' fetch first row only) and remote_port = 0`);
+    const rows = await connection.runSQL(`select job_name, local_port from qsys2.netstat_job_info j where job_name = (select job_name from qsys2.netstat_job_info j where local_port = ${connection.config?.debugPort || 8005} and remote_address = '0.0.0.0' fetch first row only) and remote_address = '0.0.0.0'`);
     if (rows && rows.length) {
       return {
         name: String(rows[0].JOB_NAME),
