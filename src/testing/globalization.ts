5137d18d9f3fff689129ebfc3d0988137cbf8b78
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2024-06-19 12:38:05 -0400

    remove change to user profile CCSID
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/testing/globalization.ts
+++ src/testing/globalization.ts
@@ -22,14 +22,7 @@ export const GlobalizationSuite: TestSuite = {
       name: `CCSID ${ccsid}`, test: async () => {
         const connection = instance.getConnection();
         const content = instance.getContent();
-
         const test = testData[ccsid as keyof typeof testData];
-
-        // Temporarily update user profile default CCSID
-        const [originalUserDefaultCCSID] = await content!.ibmi.runSQL(`select CHARACTER_CODE_SET_ID from table( QSYS2.QSYUSRINFO( USERNAME => upper('${content!.ibmi.currentUser}') ) )`);
-        const newCssidRes = await connection!.runCommand({ command: `CHGUSRPRF USRPRF(${content!.ibmi.currentUser}) CCSID(${ccsid})`, noLibList: true });
-        const [newUserDefaultCCSID] = await content!.ibmi.runSQL(`select CHARACTER_CODE_SET_ID from table( QSYS2.QSYUSRINFO( USERNAME => upper('${content!.ibmi.currentUser}') ) )`);
-        assert.strictEqual(newUserDefaultCCSID.CHARACTER_CODE_SET_ID, Number(ccsid));
 
         const crtlibRes = await connection!.runCommand({ command: `CRTLIB LIB(${test.library}) TEXT(${test.libraryText})`, noLibList: true, environment: "ile" });
         const crtsrcpfRes = await connection!.runCommand({ command: `CRTSRCPF FILE(${test.library}/${test.object}) RCDLEN(112) CCSID(${ccsid}) TEXT(${test.objectText})`, noLibList: true, environment: "ile" });
@@ -38,11 +31,6 @@ export const GlobalizationSuite: TestSuite = {
         const objects = await content!.getObjectList({ library: test.library, object: test.object });
         const members = await content!.getMemberList({ library: test.library, sourceFile: test.object, members: test.member });
         const dltLib = await connection!.runCommand({ command: `DLTLIB LIB(${test.library})`, noLibList: true, environment: "ile" });
-
-        // Restore user profile default CCSID
-        const restoreCcsidRes = await connection!.runCommand({ command: `CHGUSRPRF USRPRF(${content!.ibmi.currentUser}) CCSID(${originalUserDefaultCCSID.CHARACTER_CODE_SET_ID})`, noLibList: true });
-        const [restoredUserDefaultCCSID] = await content!.ibmi.runSQL(`select CHARACTER_CODE_SET_ID from table( QSYS2.QSYUSRINFO( USERNAME => upper('${content!.ibmi.currentUser}') ) )`);
-        assert.strictEqual(restoredUserDefaultCCSID.CHARACTER_CODE_SET_ID, originalUserDefaultCCSID.CHARACTER_CODE_SET_ID);
 
         assert.strictEqual(libraries.length, 1);
         assert.strictEqual(libraries[0].library, test.library);
