8f0418745b23135d77f89f6d1172bd08356babcd
Seb Julliand <sebjulliand@gmail.com>
2024-07-08 23:26:14 +0200

    Moved checking of Java feature to the cached section
    Signed-off-by: Seb Julliand <sebjulliand@gmail.com>

--- src/api/IBMi.ts
+++ src/api/IBMi.ts
@@ -578,17 +578,17 @@ export default class IBMi {
                 console.log(e);
               }
             }
-          }
 
-          //Specific Java installations check
-          progress.report({
-            message: `Checking installed components on host IBM i: Java`
-          });
-          const javaCheck = async (root: string) => await this.content.testStreamFile(`${root}/bin/java`, 'x') ? `${root}` : undefined;
-          this.remoteFeatures.jdk80 = await javaCheck(`/QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit`);
-          this.remoteFeatures.jdk11 = await javaCheck(`/QOpenSys/QIBM/ProdData/JavaVM/jdk11/64bit`);          
-          this.remoteFeatures.jdk17 = await javaCheck(`/QOpenSys/QIBM/ProdData/JavaVM/jdk17/64bit`);
-          this.remoteFeatures.openjdk11 = await javaCheck(`/QOpensys/pkgs/lib/jvm/openjdk-11`);
+            //Specific Java installations check
+            progress.report({
+              message: `Checking installed components on host IBM i: Java`
+            });
+            const javaCheck = async (root: string) => await this.content.testStreamFile(`${root}/bin/java`, 'x') ? `${root}` : undefined;
+            this.remoteFeatures.jdk80 = await javaCheck(`/QOpenSys/QIBM/ProdData/JavaVM/jdk80/64bit`);
+            this.remoteFeatures.jdk11 = await javaCheck(`/QOpenSys/QIBM/ProdData/JavaVM/jdk11/64bit`);
+            this.remoteFeatures.jdk17 = await javaCheck(`/QOpenSys/QIBM/ProdData/JavaVM/jdk17/64bit`);
+            this.remoteFeatures.openjdk11 = await javaCheck(`/QOpensys/pkgs/lib/jvm/openjdk-11`);
+          }
 
           if (this.sqlRunnerAvailable()) {
             //Temporary function to run SQL
