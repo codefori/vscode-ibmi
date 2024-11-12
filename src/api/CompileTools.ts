ed2d58f01dd6678789ad5a629b8171dbceb75b2c
worksofliam <mrliamallan@live.co.uk>
2024-11-12 17:23:45 -0500

    Print the command
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/api/CompileTools.ts
+++ src/api/CompileTools.ts
@@ -682,7 +682,7 @@ export namespace CompileTools {
               command: [
                 ...options.noLibList? [] : buildLiblistCommands(connection, ileSetup),
                 ...commands.map(command =>
-                  `${`system ${GlobalConfiguration.get(`logCompileOutput`) ? `` : `-s`} "${IBMi.escapeForShell(connection.sysNameInAmerican(command))}"`}`,
+                  `${`system -v "${IBMi.escapeForShell(connection.sysNameInAmerican(command))}"`}`,
                 )
               ].join(` && `),
               directory: cwd,
