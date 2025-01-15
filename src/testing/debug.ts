bb837340f3e180bb5376331bc1c1d7d8fc710af2
worksofliam <mrliamallan@live.co.uk>
2025-01-15 11:17:39 -0500

    Fix test for Java version check
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/testing/debug.ts
+++ src/testing/debug.ts
@@ -24,7 +24,7 @@ export const DebugSuite: TestSuite = {
           assert.strictEqual(jdk11, connection.remoteFeatures.jdk17);
         }
 
-        assert.throws(() => getJavaHome(connection, '666'));
+        assert.strictEqual(getJavaHome(connection, '666'), undefined);
       }
     }
   ]
