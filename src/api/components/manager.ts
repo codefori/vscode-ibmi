27aa437e64063fd10ed6a768225fa60e5ecc13b8
worksofliam <mrliamallan@live.co.uk>
2025-02-20 23:03:49 -0500

    Make sure component manager is checking the component if the stored state is NotChecked
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/api/components/manager.ts
+++ src/api/components/manager.ts
@@ -62,7 +62,7 @@ export class ComponentManager {
       const installed = lastInstalled.find(i => i.id.name === component.getIdentification().name);
       const sameVersion = installed && (installed.id.version === component.getIdentification().version);
 
-      if (!installed || !sameVersion) {
+      if (!installed || !sameVersion || installed.state === `NotChecked`) {
         await newComponent.check();
       } else {
         newComponent.overrideState(installed.state);
