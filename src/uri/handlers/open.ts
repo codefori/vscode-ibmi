8652fd07c3290c9ba0ada13acc4ee5768ede3a93
Seb Julliand <sebjulliand@gmail.com>
2025-03-28 22:40:33 +0100

    Added support for user query variable
    Signed-off-by: Seb Julliand <sebjulliand@gmail.com>

--- src/uri/handlers/open.ts
+++ src/uri/handlers/open.ts
@@ -17,12 +17,13 @@ type Parameters = {
  * Handles /open with the following query parameters: 
  *  - `path`: an IFS path; supports /QSYS.LIB paths to open members
  *  - `host` (optional): the IBM i host to connect to; can be a host name, an IP or a server configuration name use current connection if not specified
+ *  - `user`(optional): if specified with host, a connection with the specified user must exist
  *  - `readonly`: if specified, the member/file will be opened in read-only mode
  * 
  * Examples:
  * - /open?path=/tmp/test.txt
  * - /open?path=/tmp/dontchange.txt&readonly
- * - /open?host=PUB400.com&path=/tmp/dontchange.txt&readonly
+ * - /open?host=PUB400.com&user=JOHNDOE&path=/tmp/dontchange.txt&readonly
  */
 export const openURIHandler: Code4iUriHandler = {
   canHandle: (path) => path === `/open`,
@@ -32,7 +33,7 @@ export const openURIHandler: Code4iUriHandler = {
       if (!parameters.cancel) {
         if (parameters.connect && parameters.host) {
           await vscode.commands.executeCommand(`code-for-ibmi.connectTo`, parameters.host.name)
-        }        
+        }
 
         vscode.commands.executeCommand("code-for-ibmi.openWithDefaultMode", parameters.path, parameters.readonly);
       }
@@ -63,7 +64,8 @@ function toBoolean(value?: string) {
 async function loadParameters(query: querystring.ParsedUrlQuery, connection?: IBMi): Promise<Parameters> {
   const path = toPath(query.path as string | undefined);
   const readonly: DefaultOpenMode | undefined = toBoolean(query.readonly as string | undefined) ? "browse" : undefined;
-  const connectionData = await resolveConnectionData(query.host as string | undefined);
+  const user = (query.user as string)?.toLocaleUpperCase();
+  const connectionData = await resolveConnectionData(query.host as string | undefined, user);
   let connect;
   let cancel;
 
@@ -72,8 +74,10 @@ async function loadParameters(query: querystring.ParsedUrlQuery, connection?: IB
   }
 
   if (connection && connectionData) {
-    if (connectionData.name !== connection.currentConnectionName) {
-      if (await vscode.window.showWarningMessage(l10n.t("You're currently connected to {0}. Do you want to disconnect and switch to {1}?", connection.currentHost, connectionData.name), { modal: true }, l10n.t("Connect to {0}", connectionData.name))) {
+    if (connectionData.name !== connection.currentConnectionName || (user && connection.currentUser.toLocaleUpperCase() !== user)) {
+      const message = user ? l10n.t("You're currently connected to {0} with user profile {1}. Do you want to disconnect and switch to {2} with user profile {3}?", connection.currentHost, connection.currentUser, connectionData.name, user) :
+        l10n.t("You're currently connected to {0}. Do you want to disconnect and switch to {1}?", connection.currentHost, connectionData.name);
+      if (await vscode.window.showWarningMessage(message, { modal: true }, l10n.t("Connect to {0}", connectionData.name))) {
         connect = true;
       }
       else {
@@ -95,21 +99,27 @@ async function loadParameters(query: querystring.ParsedUrlQuery, connection?: IB
  * @returns the corresponding {@link ConnectionData}
  * @throws an `Error` if DNS lookup fails or if no configuration matches this host
  */
-async function resolveConnectionData(host?: string) {
+async function resolveConnectionData(host?: string, user?: string) {
   if (host) {
+    const userMatches = (connectionData: ConnectionData) => !user || connectionData.username.toLocaleUpperCase() === user;
     const connectionByName = IBMi.connectionManager.getByName(host)?.data;
-    if (connectionByName) {
+    if (connectionByName && userMatches(connectionByName)) {
       return connectionByName;
     }
 
     const ip = !/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(host) ? (await dns.lookup(host)).address : host;
     for (const connection of IBMi.connectionManager.getAll()) {
-      if ((await dns.lookup(connection.host)).address === ip) {
+      if ((await dns.lookup(connection.host)).address === ip && userMatches(connection)) {
         return connection;
       }
     }
 
-    throw new Error(l10n.t("No connection matches name or host {0} ({1})", host, ip));
+    if (user) {
+      throw new Error(l10n.t("No connection matches name or host {0} ({1}) with user {2}", host, ip, user));
+    }
+    else {
+      throw new Error(l10n.t("No connection matches name or host {0} ({1})", host, ip));
+    }
   }
 }
 
