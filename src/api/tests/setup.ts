9659967474fa614b853b27eddee26363555fb716
worksofliam <mrliamallan@live.co.uk>
2025-10-29 00:07:41 -0400

    Revert back
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/api/tests/setup.ts
+++ src/api/tests/setup.ts
@@ -9,19 +9,21 @@ export async function setup(project: TestProject) {
   // When the config files exist, it makes future connections just slightly faster.
   // Mostly useful during the CI stage.
 
-    const configsExist = exists(JSONConfig.NAME) && exists(JsonStorage.NAME);
+    // const configsExist = exists(JSONConfig.NAME) && exists(JsonStorage.NAME);
 
-    if (configsExist) {
-      console.log(`Connection settings already existing. Skipped connection setup.`);
-    } else {
-      console.log(``);
-      console.log(`Connecting before tests run to create/refresh settings.`);
-      const conn = await newConnection(true);
-      await disposeConnection(conn);
+    // if (configsExist) {
+    //   console.log(`Connection settings already existing. Skipped connection setup.`);
+    // } else {
 
-      console.log(`Testing connection complete. Settings written/refreshed.`);
-      console.log(``);
-    }
+    // }
+
+    console.log(``);
+    console.log(`Connecting before tests run to create/refresh settings.`);
+    const conn = await newConnection(true);
+    await disposeConnection(conn);
+
+    console.log(`Testing connection complete. Settings written/refreshed.`);
+    console.log(``);
 }
 
 function exists(fileName: string) {
