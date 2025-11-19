a4dbd63e74f9a1e0f2c6bfea44e3d70de79e6568
Christian Jorgensen <chr.jorgensen1@gmail.com>
2025-11-19 15:39:56 +0100

    Use `replaceAll` for better performance
    Co-authored-by: Sébastien Julliand <sebjulliand@gmail.com>

--- src/ui/Tools.ts
+++ src/ui/Tools.ts
@@ -104,11 +104,11 @@ export namespace VscodeTools {
 
   export function escapeHtml(html: string) {
     return html
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;');
+      .replaceAll("&", '&amp;')
+      .replaceAll("<", '&lt;')
+      .replaceAll(">", '&gt;')
+      .replaceAll('"', '&quot;')
+      .replaceAll("'", '&#39;');
   }
 
 
