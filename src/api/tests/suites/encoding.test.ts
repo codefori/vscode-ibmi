53f1b802e145561932698776abcebd37c39b5d31
Julia Yan <julia.yan@ibm.com>
2025-12-02 04:53:47 -0500

    Fix tests that are now invalid

--- src/api/tests/suites/encoding.test.ts
+++ src/api/tests/suites/encoding.test.ts
@@ -60,7 +60,7 @@ describe('Encoding tests', { concurrent: true }, () => {
     await disposeConnection(connection);
   });
 
-  it('Prove that input strings are messed up by CCSID', async () => {
+  it('Prove that input strings are NO LONGER messed up by CCSID', async () => {
     let howManyTimesItMessedUpTheResult = 0;
 
     for (const strCcsid in contents) {
@@ -80,7 +80,7 @@ describe('Encoding tests', { concurrent: true }, () => {
       }
     }
 
-    expect(howManyTimesItMessedUpTheResult).toBeTruthy();
+    expect(howManyTimesItMessedUpTheResult).toBe(0);
   });
 
   it('Compare Unicode to EBCDIC successfully', async () => {
