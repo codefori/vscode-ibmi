bdfa3b72a932d44b6da1ee927bd6f0e66ee702ce
worksofliam <mrliamallan@live.co.uk>
2026-01-23 14:08:30 -0500

    Update Mapepire version
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/api/components/mapepire/version.ts
+++ src/api/components/mapepire/version.ts
@@ -1,5 +1,5 @@
 
-export const VERSION = `2.3.3`;
-export const VERSION_NUMBER = 233;
+export const VERSION = `2.3.4`;
+export const VERSION_NUMBER = 234;
 export const SERVER_VERSION_TAG = `v${VERSION}`;
 export const SERVER_VERSION_FILE = `mapepire-server-${VERSION}.jar`;
