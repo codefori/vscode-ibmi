9567dd39a34692db7c39f4e1da54faf718f8ca40
Seb Julliand <sebjulliand@gmail.com>
2026-06-03 18:01:06 +0200

    Try to put back iasp tests
    Signed-off-by: Seb Julliand <sebjulliand@gmail.com>

---
+++ src/api/tests/suites/asp.test.ts
@@ -0,0 +1,97 @@
+import assert from "assert";
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import IBMi from "../../IBMi";
+import { SearchTools } from "../../SearchTools";
+import { Tools } from "../../Tools";
+import { CONNECTION_TIMEOUT, disposeConnection, newConnection } from "../connection";
+
+const SPFNAME = `VSCODESPFT`;
+const MBRNAME = `VSCODEMBRT`;
+
+async function createTestASPLibrary(connection: IBMi) {
+  //List all iASPS
+  const asps = (await connection.runSQL("select OBJNAME from table(QSYS2.OBJECT_STATISTICS('QSYS', '*DEVD')) where OBJATTRIBUTE = 'ASP'"))
+    .map(row => String(row.OBJNAME));
+
+  if (asps.length > 0) {
+    //Create the test library on the first active iasp
+    for (const asp of asps) {
+      const tempLibName = Tools.makeid();
+      const res = await connection.runCommand({ command: `QSYS/CRTLIB LIB(${tempLibName}) ASPDEV(${asp})` });
+      if (res.code === 0) {
+        assert.strictEqual(await connection.getLibraryIAsp(tempLibName), asp, "Temp library ASP doesn't match the expected ASP.")
+        return tempLibName;
+      }
+      else {
+        console.log(`Can't create library on iASP ${asp}: ${res.stderr || res.stdout}`);
+      }
+    }
+  }
+}
+
+async function createTempRpgle(connection: IBMi, library: string) {
+  const content = connection.getContent();
+
+  await connection.runCommand({
+    command: `QSYS/CRTSRCPF ${library}/${SPFNAME} MBR(*NONE)`,
+    environment: `ile`
+  });
+
+  await connection.runCommand({
+    command: `QSYS/ADDPFM FILE(${library}/${SPFNAME}) MBR(${MBRNAME}) `,
+    environment: `ile`
+  });
+
+  const baseContent = `**FREE\ndsply 'hello world';`;
+
+  return await content?.uploadMemberContent(library, SPFNAME, MBRNAME, baseContent);
+}
+
+describe(`iASP tests`, { concurrent: true }, () => {
+  let connection: IBMi
+  let skipAsp = false;
+  let tempLib: string | undefined;
+  beforeAll(async () => {
+    connection = await newConnection();
+    tempLib = await createTestASPLibrary(connection);
+    if (tempLib) {
+      await createTempRpgle(connection, tempLib);
+    } else {
+      console.log(`Skipping iASP tests, no iASP is available.`);
+      skipAsp = true;
+    }
+  }, CONNECTION_TIMEOUT)
+
+  afterAll(async () => {
+    await connection.runCommand({ command: `QSYS/DLTLIB LIB(${tempLib})` });
+    await disposeConnection(connection);
+  });
+
+  beforeEach((t) => {
+    if (skipAsp) {
+      t.skip();
+    }
+  });
+
+  it('Read members in ASP and base', async () => {
+    const aspMbrContents = await connection.getContent()?.downloadMemberContent(tempLib!, SPFNAME, MBRNAME);
+
+    assert.ok(aspMbrContents);
+  });
+
+  it('can find ASP members via search', async () => {
+    const searchResults = await SearchTools.searchMembers(connection, tempLib!, SPFNAME, `hello world`, `*`);
+    expect(searchResults.hits.length).toBeGreaterThan(0);
+    // TODO: additional expects
+  });
+
+  it('can resolve member info from ASP', async () => {
+    const resolved = await connection.getContent().memberResolve(MBRNAME, [
+      { library: `QSYS`, name: `QSYSINC` },
+      { library: tempLib!, name: SPFNAME }
+    ]);
+
+    expect(resolved).toBeDefined();
+    //TODO: additional expects
+  });
+});
