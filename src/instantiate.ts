11606d809ca16e5a1c48b4567eaf17bb194c80db
Nicolas Schindler <schindler.nicolas@gmx.de>
2026-07-04 13:02:10 +0200

    fixed typo in job info bar text
    Signed-off-by: Nicolas Schindler <schindler.nicolas@gmx.de>

--- src/instantiate.ts
+++ src/instantiate.ts
@@ -156,7 +156,7 @@ async function updateJobInfoBar() {
 
     jobInfoBarItem.text = `$(server-environment) ${sqlJobId}`;
     const jobInfoBarItemTooltips: String[] = [];
-    const sqlJobInfo = sqlJobId ? `[$(server-process) Job info](command:code-for-ibmi.showIBMiJobInfo)` : ``;
+    const sqlJobInfo = sqlJobId ? `[$(server-process) Job Info](command:code-for-ibmi.showIBMiJobInfo)` : ``;
     jobInfoBarItemTooltips.push(
       jdbcInfo,
       sqlJobInfo,
