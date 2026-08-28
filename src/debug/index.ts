a59193f74c7a1988343204f75b9916fbe8009ec0
Seb Julliand <sebjulliand@gmail.com>
2026-08-28 08:53:38 +0200

    Ensure debug uses *CURLIB if no current library is set
    Signed-off-by: Seb Julliand <sebjulliand@gmail.com>

--- src/debug/index.ts
+++ src/debug/index.ts
@@ -419,7 +419,7 @@ export async function startDebug(instance: Instance, options: DebugOptions) {
         "subType": "batch",
         "library": options.library.toUpperCase(),
         "program": options.object.toUpperCase(),
-        "startBatchJobCommand": `SBMJOB CMD(${currentCommand}) INLLIBL(${options.libraries.libraryList.join(` `)}) CURLIB(${options.libraries.currentLibrary}) JOBQ(QSYSNOMAX) MSGQ(*USRPRF) CPYENVVAR(*YES)`,
+        "startBatchJobCommand": `SBMJOB CMD(${currentCommand}) INLLIBL(${options.libraries.libraryList.join(` `)}) CURLIB(${Tools.getCurLib(options.libraries.currentLibrary)}) JOBQ(QSYSNOMAX) MSGQ(*USRPRF) CPYENVVAR(*YES)`,
         "updateProductionFiles": updateProductionFiles,
         "trace": enableDebugTracing,
       } as vscode.DebugConfiguration;
